a1e72ac93f938bffdb4b9c7d697379ded939acef
zhuchentong <2037630@163.com>
2021-09-01 16:29:05 +0800

    feat: 更新get方法

--- src/shared/common/get.ts
+++ src/shared/common/get.ts
@@ -1,8 +1,10 @@
 import { MaybeRef } from '@vueuse/core'
 import { unref } from 'vue'
 
-export function get<T>(obj: MaybeRef<T>, key?: keyof T) {
-  if (key == null) return unref(obj)
+export function get<T>(target: MaybeRef<T>): T
+export function get<T>(target: MaybeRef<T>, key: keyof T): T
+export function get<T>(target: MaybeRef<T>, key?: keyof T) {
+  if (key == null) return unref(target)
 
-  return unref(obj)[key]
+  return unref(target)[key]
 }
